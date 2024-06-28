ee1e95cf7aa6b1f7832837bc18625fd5540e110e
Olamzzy913 <emmanuelojelade7@gmail.com>
2024-06-28 09:56:46 +0100

    update index file

--- pages/chat.js
+++ pages/chat.js
@@ -674,7 +674,7 @@ export default function Messages() {
                   <div className="ml-4">
                     <p className="capitalised font-bold">{selectedUser}</p>
                     <p className="">
-                      {isSelectedUserActive.state === "Offline"
+                      {isSelectedUserActive.state === "Online"
                         ? isSelectedUserActive.state
                         : "last seen " +
                           formatTimestamp(isSelectedUserActive.lastChanged)}
